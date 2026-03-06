8eba73b38677af179ff2ba05b571dd93cd181383
JinYong Lee <imaekdal.kor@gmail.com>
2026-03-07 00:21:17 +0900

    Change for MidNight

--- ShowK.lua
+++ ShowK.lua
@@ -1,29 +1,21 @@
 local frame = CreateFrame("Frame", "ShowKFrame", UIParent)
-frame:SetSize(55, 40)  -- 표시할 영역의 크기 설정
-frame:SetPoint("CENTER", UIParent, "CENTER", -1312, -185)  -- 화면 중앙에서 오른쪽으로 200, 위로 100 픽셀 위치에 배치
+frame:SetSize(6, 6)  -- 표시할 영역의 크기 설정
+frame:SetPoint("CENTER", UIParent, "CENTER", -1403, -237)  -- 설정된 픽셀 위치에 배치
 
 -- 배경 텍스처 추가 및 설정
 frame.bg = frame:CreateTexture(nil, "BACKGROUND")
 frame.bg:SetAllPoints(frame)
-frame.bg:SetColorTexture(0, 0, 0, 1)  -- 초기 상태는 불투명 (검은색 배경)
-
-local text = frame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-text:SetFont("Fonts\\FRIZQT__.TTF", 30, "OUTLINE")  -- 폰트 설정 (폰트 파일 경로, 크기, 스타일)
-text:SetText("K")
-text:SetAllPoints(frame)
-text:Show()  -- 초기 상태는 보이는 상태
+frame.bg:SetColorTexture(1, 1, 1, 1)  -- 초기 상태는 불투명 (하얀색 배경)
 
 local isKShown = true
 local isChanneling = false
 
 local function ShowK()
-    text:Show()
-    frame.bg:SetColorTexture(0, 0, 0, 1)
+    frame.bg:SetColorTexture(1, 1, 1, 1)
 end
 
 local function HideK()
-    text:Hide()
-    frame.bg:SetColorTexture(0, 0, 0, 0)
+    frame.bg:SetColorTexture(1, 1, 1, 0)
 end
 
 local function ToggleKDisplay()
